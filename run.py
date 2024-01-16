79aae9b2615acdc1e3f3a6bcf98315d655d18e7a
Wisered <arthur.saunier0@gmail.com>
2024-01-17 00:34:55 +0100

    removed a python file

--- run.py
+++
@@ -1,9 +0,0 @@
-import cProfile
-from app import app
-
-if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
-    app.run()
-    profiler.disable()
-    profiler.dump_stats("flask_startup_profile.pstat")
